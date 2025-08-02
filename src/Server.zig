3cdea4384e73d15f4fdaa22bde704e928ce8b76d
Samuel Dasari <samueldasari@samuels-air.mynetworksettings.com>
2025-08-02 16:16:12 -0400

    Auto-commit: Sat Aug  2 16:16:12 2025

--- src/Server.zig
+++ src/Server.zig
@@ -44,7 +44,7 @@ fn handleClient(connection: net.Server.Connection) void {
     }
 
     const player_id = player_count;
-    players[player_id] = Player.init(0.0, 0.0);
+    players[player_id] = creat
     player_count += 1;
 
     std.debug.print("Player {} connected\n", .{player_id});
