47fe884eba93741ce5114b72c3ace3d625aa7ed4
Samuel Dasari <samueldasari@samuels-air.mynetworksettings.com>
2025-08-02 17:05:32 -0400

    Auto-commit: Sat Aug  2 17:05:32 2025

--- src/Server.zig
+++ src/Server.zig
@@ -6,8 +6,8 @@ const Thread = std.Thread;
 
 const MAX_PLAYERS = 64;
 
-var players: [MAX_PLAYERS]Player = undefined;
-var player_count: usize = 0;
+var player_count: usize = 0var players: [MAX_PLAYERS]?Player = [_]?Player{null} ** MAX_PLAYERS;
+;
 var mutex = Thread.Mutex{};
 
 pub fn startServer() !void {
