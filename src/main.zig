54f31aa734e35c134445d314818b1224c24dc444
Samuel Dasari <samueldasari@samuels-air.mynetworksettings.com>
2025-08-03 17:36:04 -0400

    Auto-commit: Sun Aug  3 17:36:04 2025

--- src/main.zig
+++ src/main.zig
@@ -262,7 +262,7 @@ const GameInstance = struct {
 
         const UpdateFunctions = struct {
             last_input: ?u8 = null,
-            world: *Engine.World = &world,
+            world: *instance.World = &world,
             fn update() void {
                 if (current_instance) |instance| {
                     instance.mutex.lock();
