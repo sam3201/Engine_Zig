4edb3f72f2025b4f060f210a204300e6588dc199
Samuel Dasari <samueldasari@samuels-air.mynetworksettings.com>
2025-08-04 09:42:16 -0400

    Auto-commit: Mon Aug  4 09:42:16 2025

--- src/main.zig
+++ src/main.zig
@@ -64,10 +64,11 @@ pub fn main() !void {
     while (title_engine.running) {
         title_engine.clock.tick();
 
-            title_engine.running if ((try Engine.readKey()) != null) {
-= false;
+        if (try Engine.readKey() != false) {
+            title_engine.running = false;
             break;
-        }
+}
+
 
         title_engine.canvas.clear(' ', Engine.Color{ .r = 10, .g = 10, .b = 10 });
         UpdateFunctions.update(&title_engine.canvas);
