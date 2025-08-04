4e9c60e77a465cabf93919dec193c903eb150ca8
Samuel Dasari <samueldasari@samuels-air.mynetworksettings.com>
2025-08-04 09:43:55 -0400

    Auto-commit: Mon Aug  4 09:43:55 2025

--- src/Client.zig
+++ src/Client.zig
@@ -98,9 +98,8 @@ pub fn main() !void {
     var stream = try connectToServer();
     defer disconnectFromServer(&stream);
 
-    const current_stream: ?*net.Stream = &stream;
-
     const UpdateFunctions = struct {
+    const current_stream: ?*net.Stream = &stream;
         fn update(canvas: *eng.Canvas) void {
             if (current_stream) |s| {
                 renderGameState(s, allocator, canvas) catch |err| {
