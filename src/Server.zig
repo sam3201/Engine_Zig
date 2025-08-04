415a92e2c9bb4d2f7cf8d92cb89836048b743513
Samuel Dasari <samueldasari@samuels-air.mynetworksettings.com>
2025-08-04 18:48:53 -0400

    Auto-commit: Mon Aug  4 18:48:53 2025

--- src/Server.zig
+++ src/Server.zig
@@ -114,6 +114,7 @@ pub const GameServer = struct {
                 Self.server.world_manager.draw();
                 drawServerOverview(canvas, Self.server);
             }
+            self.updateFn = update;
         };
 
         const context = ServerContext{ .server = self, .updateFn = self.server_engine.updateFn };
