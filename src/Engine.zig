2b671656a2dd86ffc8c86f073f1ca5b15de03ccc
Samuel Dasari <samueldasari@samuels-air.mynetworksettings.com>
2025-08-04 20:27:24 -0400

    Auto-commit: Mon Aug  4 20:27:24 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -191,7 +191,7 @@ pub const Canvas = struct {
         }
     }
 
-    pub fn setUpdateFn(Self: *@This(), update_fn: UpdateFn) void {
+    pub fn setUpdateFn(self: *@This(), update_fn: UpdateFn) void {
         self.updateFn = update_fn;
     }
 
