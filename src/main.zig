3b505e88cc91802aee6991b3328a1033aeabfe38
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-09 18:45:14 -0400

    Auto-commit: Sat Aug  9 18:45:14 2025

--- src/main.zig
+++ src/main.zig
@@ -66,7 +66,7 @@ pub fn main() !void {
     while (title_engine.running) {
         title_engine.clock.tick();
 
-        if (try Engine.readKey() key != null) {
+        if (try Engine.readKey() != null) {
             title_engine.running = false;
             break;
         }
