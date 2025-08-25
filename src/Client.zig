941c2b6a865f5a4633a4a7d569e2192a2b8ad152
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-25 14:23:26 -0400

    Auto-commit: Mon Aug 25 14:23:26 2025

--- src/Client.zig
+++ src/Client.zig
@@ -119,6 +119,7 @@ pub fn main() !void {
 
     var stream = try connectToServer();
     g_stream = &stream;
+    g_allocator = allocator;
     defer disconnectFromServer(&stream);
 
     engine.canvas.setUpdateFn(update);
