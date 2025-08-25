fe433659828fa94cedf83c63173121cb77d794e2
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-25 14:21:48 -0400

    Auto-commit: Mon Aug 25 14:21:48 2025

--- src/Server.zig
+++ src/Server.zig
@@ -123,7 +123,7 @@ pub const GameServer = struct {
     }
 
     fn runServerEngine(self: *GameServer) void {
-        global_server = self; 
+        global_server = self;
         self.server_engine.canvas.setUpdateFn(updateCallback);
         self.server_engine.run() catch |err| {
             std.debug.print("Server engine error: {}\n", .{err});
