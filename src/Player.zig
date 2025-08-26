233bd94aba47aedec68d557d220e1b2913754ce6
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-25 23:53:01 -0400

    Auto-commit: Mon Aug 25 23:53:01 2025

--- src/Player.zig
+++ src/Player.zig
@@ -247,7 +247,7 @@ pub fn load(allocator: std.mem.Allocator, path: []const u8) !Player {
 
     var name = obj.get("name").?.string;
     const health: i32 = @intCast(obj.get("health").?.integer);
-    const max_health = @intCast(i32, obj.get("max_health").?.integer);
+    const max_health: i32 = @intCast(obj.get("max_health").?.integer);
     const xp = @intCast(i32, obj.get("xp").?.integer);
     const level = @intCast(i32, obj.get("level").?.integer);
     const experience = @intCast(i32, obj.get("experience").?.integer);
