e4b224d2b549e9ee54b09a6dfe6a9c284e633684
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-26 09:31:43 -0400

    Auto-commit: Tue Aug 26 09:31:43 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -131,7 +131,7 @@ pub const Chunk = struct {
             .tiles = tiles,
             .biome = .Plains,
             .difficulty_level = difficulty_level,
-        .items = items };
+            .items = items,
         };
     }
 
