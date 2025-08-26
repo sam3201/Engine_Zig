ba75a7a93771707e241284e26e861eebf7093b8c
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-26 09:49:44 -0400

    Auto-commit: Tue Aug 26 09:49:44 2025

--- src/Server.zig
+++ src/Server.zig
@@ -31,7 +31,7 @@ pub const GameServer = struct {
     pub fn init(allocator: std.mem.Allocator) !GameServer {
         const canvas = try Engine.Canvas.init(allocator, 80, 24);
         const host_player = try PlayerModule.createWASDPlayer(allocator, 30, 15);
-        var world_manager = try WorldManager.WorldManager.init(allocator, canvas, host_player);
+        var world_manager = try WorldManager.WorldManager.init(allocator, &canvas, host_player);
 
         // Generate initial chunks
         try world_manager.updateChunks();
