f032984ddbbac838d85cc36bfac17f255fad9983
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-26 09:19:40 -0400

    Auto-commit: Tue Aug 26 09:19:40 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -111,6 +111,8 @@ pub const Chunk = struct {
 
     pub fn init(coord: ChunkCoord, difficulty_level: i32, allocator: std.mem.Allocator) Chunk {
         var items = std.ArrayList(eng.Item).init(allocator);
+        try items.append(Item.init(1, "Potion", 1));
+        return .{ .coord = coord, .difficulty_level = difficulty, .items = items };
 
         var tiles: [CHUNK_SIZE * CHUNK_SIZE]TileType = undefined;
 
