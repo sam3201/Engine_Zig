e220c1842df1a4f2cdc84645240d4f506930b7ca
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-25 23:50:53 -0400

    Auto-commit: Mon Aug 25 23:50:53 2025

--- src/Player.zig
+++ src/Player.zig
@@ -142,7 +142,7 @@ pub const PlayerData = struct {
     experience: i32,
     experience_to_next_level: i32,
     key_bindings: []KeyBinding,
-    inventory: [],
+    inventory: []u8,
     // TODO: inventory []Item when we add inventory
 };
 
