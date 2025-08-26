64fdf8c92bc05d4908f848f1c28146e16ee29777
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-26 09:08:42 -0400

    Auto-commit: Tue Aug 26 09:08:42 2025

--- src/Player.zig
+++ src/Player.zig
@@ -129,9 +129,7 @@ pub const Player = struct {
         return self.level;
     }
 
-    pub fn addItem(self: *Player, item: Item) void {
-        
-    }
+    pub fn addItem(self: *Player, item: Item) void {}
 
     pub fn draw(self: Player, canvas: *eng.Canvas) void {
         canvas.put(self.entity.x, self.entity.y, self.entity.ch);
