5680678e539fde1e5e19e8d28bc777c810364755
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-26 20:11:20 -0400

    Auto-commit: Tue Aug 26 20:11:20 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -254,13 +254,14 @@ pub const Engine = struct {
         while (self.running) {
             self.clock.tick();
 
-            if (try readKey()) |byte| {
-                if (byte == 'q' or byte == 27) {
-                    self.running = false;
-                    break;
-                }
-                main.setInput(byte);
-            }
+            
+            //if (try readKey()) |byte| {
+            //    if (byte == 'q' or byte == 27) {
+            //        self.running = false;
+            //        break;
+            //    }
+            //    main.setInput(byte);
+            //}
 
             self.canvas.clear(
                 ' ',
