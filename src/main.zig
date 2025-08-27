a2b7bede22d992606ed4ba36c783566d7e8316bf
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-26 20:21:29 -0400

    Auto-commit: Tue Aug 26 20:21:29 2025

--- src/main.zig
+++ src/main.zig
@@ -52,6 +52,11 @@ pub fn main() !void {
     );
     defer title_engine.deinit();
 
+        var menu = InputMenu.init(&[_][]const u8{"Start Game", "Options", "Exit"});
+    const choice = menu.handle_input();
+    std.debug.print("You selected: {s}\n", .{menu.options[choice]});
+
+
     var term = try Engine.TerminalGuard.init();
     defer term.deinit();
 
