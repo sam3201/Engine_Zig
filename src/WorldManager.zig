78c6ead31f82fe6b8c7d459ec8ac45028a32089a
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-28 00:47:31 -0400

    Auto-commit: Thu Aug 28 00:47:31 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -39,7 +39,7 @@ pub const WorldManager = struct {
         };
 
         const self = &world;
-        _ = self.randomizeStart();
+        self.randomizeStart();
         try world.updateChunks();
         world.updateCamera();
 
