26bd13192d6f5bb71635ac9051f1eaa65c67ea6c
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-28 14:51:29 -0400

    Auto-commit: Thu Aug 28 14:51:29 2025

--- src/Player.zig
+++ src/Player.zig
@@ -24,7 +24,7 @@ pub const KeyBinding = struct {
 };
 
 pub const Player = struct {
-    inventory: Inventory.Inventory, 
+    inventory: Inventory.Inventory,
     entity: Entity.Entity,
     key_bindings: []KeyBinding,
     allocator: std.mem.Allocator,
