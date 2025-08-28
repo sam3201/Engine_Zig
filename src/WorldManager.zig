85bfc69092908c5d597a3125f6b9e5c3f2f527c0
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-28 09:44:38 -0400

    Auto-commit: Thu Aug 28 09:44:38 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -82,6 +82,7 @@ pub const WorldManager = struct {
 
     fn biomeForCoord(coord: Chunk.ChunkCoord, seed: u64) Chunk.BiomeType {
         var rng = std.Random.DefaultPrng.init(coord.hash() ^ seed);
+        @typeInfo(Chunk.BiomeType).EnumFieldCount;
         const r = rng.random().intRangeLessThan(u32, 0, @intCast(Chunk.BiomeType.len));
         return Chunk.BiomeType(r);
     }
