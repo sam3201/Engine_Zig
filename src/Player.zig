6e404ca109597a34fdaf83412685fc1db2ee55c8
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-28 14:51:05 -0400

    Auto-commit: Thu Aug 28 14:51:05 2025

--- src/Player.zig
+++ src/Player.zig
@@ -3,7 +3,7 @@
 const std = @import("std");
 const eng = @import("Engine.zig");
 const Entity = @import("Entity.zig");
-const Inventory = @import("Inventory.zig"); 
+const Inventory = @import("Inventory.zig");
 
 pub const InputAction = enum {
     UP,
