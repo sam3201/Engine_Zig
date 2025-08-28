d4230527dee74d5244da73aec97a0c17c3d73b43
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-28 09:55:45 -0400

    Auto-commit: Thu Aug 28 09:55:45 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -347,7 +347,7 @@ pub fn randomBiome() Chunk.BiomeType {
     const seed: u64 = @intCast(std.time.milliTimestamp());
     var prng = std.Random.DefaultPrng.init(seed);
     var rand_val = prng.random();
-    const roll = rand_val.intRangeLessThan(u32, 0, Chunk.BiomeType.count);
+    const roll = rand_val.intRangeLessThan(u32, 0, Chunk.count);
 
     return Chunk.BiomeType(roll);
 }
