272f3e0cbaa376755b847fa3f9a0106e39710b3a
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-28 00:44:00 -0400

    Auto-commit: Thu Aug 28 00:44:00 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -325,7 +325,7 @@ pub const WorldManager = struct {
         }
     }
 
-    pub fn randomizeStart() void {
+    pub fn randomizeStart(self: *WorldManager) void {
         const timestamp_u64: u64 = @intCast(std.time.nanoTimestamp());
         var prng = std.Random.DefaultPrng.init(timestamp_u64);
         var rand = prng.random();
@@ -339,5 +339,6 @@ pub const WorldManager = struct {
             4 => .Tundra,
             else => .Volcanic,
         };
+
     }
 };
