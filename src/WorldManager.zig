922194b14b812f9a2745382857815fd7e803d2ba
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-28 09:50:52 -0400

    Auto-commit: Thu Aug 28 09:50:52 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -347,7 +347,7 @@ pub fn randomBiome() Chunk.BiomeType {
     const seed: u64 = @intCast(std.time.milliTimestamp());
     var prng = std.Random.DefaultPrng.init(seed);
     var rand_val = prng.random();
-
+@typeInfo(MyEnum).@enum.fields.len;
     const biome_count = @typeInfo(Chunk.BiomeType).EnumFieldCount;
     const roll = rand_val.intRangeLessThan(u32, 0, biome_count);
 
