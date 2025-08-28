f6a1d8e12151c1e36601d0afc59b7d768e064d4c
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-28 14:56:22 -0400

    Auto-commit: Thu Aug 28 14:56:22 2025

--- src/Player.zig
+++ src/Player.zig
@@ -65,7 +65,18 @@ pub const Player = struct {
 
         pub fn createWASDPlayer(allocator: std.mem.Allocator, x: i32, y: i32) !Player {
         return Player{
-            // … your existing init …
+            .entity = Entity.Entity.init(x, y, 1, 1, Entity.RenderableType.PLAYER.toId(), '@', eng.Color{ .r = 255, .g = 255, .b = 0 }),
+            .key_bindings = &[_]KeyBinding{
+                .{ .key = 'w', .action = .UP },
+                .{ .key = 's', .action = .DOWN },
+                .{ .key = 'a', .action = .LEFT },
+                .{ .key = 'd', .action = .RIGHT },
+                .{ .key = 'e', .action = .INTERACT },
+                .{ .key = ' ', .action = .ATTACK },
+                .{ .key = 'i', .action = .OPENINVENTORY },
+            },
+            .allocator = allocator,
+            
             .inventory = try Inventory.Inventory.init(allocator),
         };
     }
