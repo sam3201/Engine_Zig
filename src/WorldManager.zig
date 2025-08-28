cb0d6c3fad75f1a3b77331c2ec7b128fc65243cf
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-28 09:13:32 -0400

    Auto-commit: Thu Aug 28 09:13:32 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -36,8 +36,8 @@ pub const WorldManager = struct {
             .chunks = std.HashMap(Chunk.ChunkCoord, Chunk.Chunk, ChunkContext, std.hash_map.default_max_load_percentage).init(allocator),
             .canvas_width = @intCast(canvas.width),
             .canvas_height = @intCast(canvas.height),
-            .biome = randomBiome(),
         };
+        const start_biome = randomBiome();
 
         try world.updateChunks();
         try world.updateCamera();
