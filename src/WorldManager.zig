70a04c586b4275cecc643668645125024bda985b
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-28 09:32:42 -0400

    Auto-commit: Thu Aug 28 09:32:42 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -40,7 +40,7 @@ pub const WorldManager = struct {
 
         const start_biome = randomBiome();
         const chunk = try Chunk.init(.{ .x = 0, .y = 0 }, start_biome, 1, allocator);
-try world.chunks.put(.{ .x = 0, .y = 0 }, chunk);
+        try world.chunks.put(.{ .x = 0, .y = 0 }, chunk);
 
         try world.updateChunks();
         try world.updateCamera();
