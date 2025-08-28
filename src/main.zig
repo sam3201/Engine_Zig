c48ae44fe37a3449924797c28221d46d5bae645a
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-28 14:32:52 -0400

    Auto-commit: Thu Aug 28 14:32:52 2025

--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,7 @@ const Menu = @import("Menu.zig").Menu;
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
-    // ───────────── Title/Menu Engine ─────────────
+    // ───────────── Engine ─────────────
     var engine = try Engine.Engine.init(
         allocator,
         80,
