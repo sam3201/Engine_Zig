eb16e17f0a47abe59de7531bbca132ef3b9658d9
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-28 09:42:08 -0400

    Auto-commit: Thu Aug 28 09:42:08 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -349,7 +349,7 @@ pub fn randomBiome() Chunk.BiomeType {
 
     const biome_count = @typeInfo(Chunk.BiomeType).Enum.fields.len;
     const roll = rand.intRangeLessThan(u32, 0, biome_count);
-return Chunk.BiomeType(roll);
+    return Chunk.BiomeType(roll);
 
     return switch (roll) {
         0 => .Plains,
