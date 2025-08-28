402a97644778c4fe7af270f7323f463f046710bf
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-28 14:12:09 -0400

    Auto-commit: Thu Aug 28 14:12:09 2025

--- src/main.zig
+++ src/main.zig
@@ -76,7 +76,7 @@ pub fn main() !void {
     var world = try WorldManager.WorldManager.init(allocator, &game_engine.canvas, player);
     defer world.deinit();
 
-    randomizeStart();
+    world.randomizeStart();
 
     while (game_engine.running and player.isAlive()) {
         game_engine.clock.tick();
