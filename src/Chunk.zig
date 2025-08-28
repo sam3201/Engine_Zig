ee62641fe47ac1b4738b7cbcf9f7c4fe552578cc
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-28 09:23:03 -0400

    Auto-commit: Thu Aug 28 09:23:03 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -120,6 +120,7 @@ pub const Chunk = struct {
     pub fn init(coord: ChunkCoord, difficulty_level: i32, allocator: std.mem.Allocator) !Chunk {
         var self = Chunk{
             .coord = coord,
+            .biome = BiomeType.Plains,
             .tiles = [_]TileType{TileType.Empty} ** (CHUNK_SIZE * CHUNK_SIZE),
             .difficulty_level = difficulty_level,
             .generated = true,
