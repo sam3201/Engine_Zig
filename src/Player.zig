ccdd0dca22fb79dea375ad740598bf5299de249e
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-28 21:58:58 -0400

    Auto-commit: Thu Aug 28 21:58:58 2025

--- src/Player.zig
+++ src/Player.zig
@@ -328,7 +328,7 @@ pub fn setKeyBinding(self: *Player, action: InputAction, key: u8) void {
 pub fn createWASDPlayer(allocator: std.mem.Allocator, x: i32, y: i32) !Player {
     return Player{
         .entity = Entity.Entity.init(x, y, 1, 1, Entity.RenderableType.PLAYER.toId(), '@', eng.Color{ .r = 255, .g = 255, .b = 0 }),
-        .hea.key_bindings = &[_]KeyBinding{
+.key_bindings = &[_]KeyBinding{
     .{ .key = 'w', .action = .UP },
     .{ .key = 's', .action = .DOWN },
     .{ .key = 'a', .action = .LEFT },
@@ -337,7 +337,8 @@ pub fn createWASDPlayer(allocator: std.mem.Allocator, x: i32, y: i32) !Player {
     .{ .key = ' ', .action = .ATTACK },
     .{ .key = 'i', .action = .OPENINVENTORY },
 }[0..],
-lth = 10,
+        },
+        .health = 10,
         .max_health = 10,
         .xp = 0,
         .speed = 3,
