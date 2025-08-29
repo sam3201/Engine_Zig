f156a86cd2030ec2ac1b438845d4c5b1e171fe23
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-28 22:00:41 -0400

    Auto-commit: Thu Aug 28 22:00:41 2025

--- src/Player.zig
+++ src/Player.zig
@@ -357,15 +357,15 @@ pub fn createWASDPlayer(allocator: std.mem.Allocator, x: i32, y: i32) !Player {
 pub fn createArrowPlayer(allocator: std.mem.Allocator, x: i32, y: i32) !Player {
     return Player{
         .entity = Entity.Entity.init(x, y, 1, 1, Entity.RenderableType.PLAYER.toId(), '@', eng.Color{ .r = 255, .g = 255, .b = 0 }),
-        .key_bindings = &[_]KeyBinding{
-            .{ .key = 'h', .action = .UP },
-            .{ .key = 'j', .action = .DOWN },
-            .{ .key = 'k', .action = .LEFT },
-            .{ .key = 'l', .action = .RIGHT },
-            .{ .key = 'e', .action = .INTERACT },
-            .{ .key = ' ', .action = .ATTACK },
-            .{ .key = 'i', .action = .OPENINVENTORY },
-        },
+.key_bindings = &[_]KeyBinding{
+    .{ .key = 'w', .action = .UP },
+    .{ .key = 's', .action = .DOWN },
+    .{ .key = 'a', .action = .LEFT },
+    .{ .key = 'd', .action = .RIGHT },
+    .{ .key = 'e', .action = .INTERACT },
+    .{ .key = ' ', .action = .ATTACK },
+    .{ .key = 'i', .action = .OPENINVENTORY },
+}[0..]
         .health = 10,
         .max_health = 10,
         .xp = 0,
