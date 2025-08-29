5c554bde6f54099578a4c2603cb8ccabd25c6659
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-28 22:01:50 -0400

    Auto-commit: Thu Aug 28 22:01:50 2025

--- src/Player.zig
+++ src/Player.zig
@@ -357,16 +357,16 @@ pub fn createWASDPlayer(allocator: std.mem.Allocator, x: i32, y: i32) !Player {
 pub fn createArrowPlayer(allocator: std.mem.Allocator, x: i32, y: i32) !Player {
     return Player{
         .entity = Entity.Entity.init(x, y, 1, 1, Entity.RenderableType.PLAYER.toId(), '@', eng.Color{ .r = 255, .g = 255, .b = 0 }),
-        .key_bindings = &[_]KeyBinding{
-            .{ .key = 'w', .action = .UP },
+        .health = 10.key_bindings = (&[_]KeyBinding{
+    .{ .key = 'w', .action = .UP },
     .{ .key = 's', .action = .DOWN },
     .{ .key = 'a', .action = .LEFT },
     .{ .key = 'd', .action = .RIGHT },
     .{ .key = 'e', .action = .INTERACT },
     .{ .key = ' ', .action = .ATTACK },
     .{ .key = 'i', .action = .OPENINVENTORY },
-}[0..]
-        .health = 10,
+})[0..],
+,
         .max_health = 10,
         .xp = 0,
         .speed = 3,
