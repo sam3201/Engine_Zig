bc6d29d805562ac9f2edb27572dea232c3165e38
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-28 22:30:07 -0400

    Auto-commit: Thu Aug 28 22:30:07 2025

--- src/Player.zig
+++ src/Player.zig
@@ -365,7 +365,7 @@ pub fn createArrowPlayer(allocator: std.mem.Allocator, x: i32, y: i32) !Player {
     return Player{
         .entity = Entity.Entity.init(x, y, 1, 1, Entity.RenderableType.PLAYER.toId(), '@', eng.Color{ .r = 255, .g = 255, .b = 0 }),
         .health = 10,
-        .key_bindings = (&[_]KeyBinding{
+        .key_bindings = ([_]KeyBinding{
             .{ .key = 'h', .action = .UP },
             .{ .key = 'j', .action = .DOWN },
             .{ .key = 'k', .action = .LEFT },
