1612900d4aa443ba5ed939c7a422948a9bed0f43
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-28 21:51:27 -0400

    Auto-commit: Thu Aug 28 21:51:27 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -1,7 +1,7 @@
 // src/Chunk.zig
 
 const std = @import("std");
-const eng = @import("Engine.zig");
+const Engine = @import("Engine.zig");
 const Inventory = @import("Inventory.zig");
 const WorldManager = @import("WorldManager.zig");
 
