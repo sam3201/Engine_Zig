f97aace816cdcb155f8be7e42e7addcb982264b2
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-28 21:59:05 -0400

    Auto-commit: Thu Aug 28 21:59:05 2025

--- src/Player.zig
+++ src/Player.zig
@@ -337,6 +337,7 @@ pub fn createWASDPlayer(allocator: std.mem.Allocator, x: i32, y: i32) !Player {
     .{ .key = ' ', .action = .ATTACK },
     .{ .key = 'i', .action = .OPENINVENTORY },
 }[0..],
+        },
         .health = 10,
         .max_health = 10,
         .xp = 0,
