4a3c4f54f514d5dd9c0c2625d666c89e6ab7de2b
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-08-29 13:14:26 -0400

    Auto-commit: Fri Aug 29 13:14:26 2025

--- src/Player.zig
+++ src/Player.zig
@@ -361,3 +361,37 @@ pub fn createWASDPlayer(allocator: std.mem.Allocator, x: i32, y: i32) !Player {
         .inventory = try Inventory.Inventory.init(allocator),
     };
 }
+
+pub fn createArrowPlayer(allocator: std.mem.Allocator, x: i32, y: i32) !Player {
+    return Player{
+        .allocator = allocator,
+        .entity = Entity.Entity.init(
+            x,
+            y,
+            1,
+            1,
+            Entity.RenderableType.PLAYER.toId(),
+            '@',
+            eng.Color{ .r = 255, .g = 255, .b = 0 },
+        ),
+        .key_bindings = @constCast((&[_]KeyBinding{
+            .{ .key = 'w', .action = .UP },
+            .{ .key = 's', .action = .DOWN },
+            .{ .key = 'a', .action = .LEFT },
+            .{ .key = 'd', .action = .RIGHT },
+            .{ .key = 'e', .action = .INTERACT },
+            .{ .key = ' ', .action = .ATTACK },
+            .{ .key = 'i', .action = .OPENINVENTORY },
+        })[0..]),
+        .name = "Player",
+        .health = 10,
+        .max_health = 10,
+        .xp = 0,
+        .speed = 3,
+        .level = 0,
+        .experience = 0,
+        .experience_to_next_level = 100,
+        .inventory = try Inventory.Inventory.init(allocator),
+    };
+}
+
