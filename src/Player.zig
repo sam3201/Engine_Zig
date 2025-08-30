5c5932a2d9250adbfecf7990d7f1107be15a25c5
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-30 13:20:09 -0400

    Auto-commit: Sat Aug 30 13:20:09 2025

--- src/Player.zig
+++ src/Player.zig
@@ -274,7 +274,7 @@ pub fn load(allocator: std.mem.Allocator, path: []const u8) !Player {
     if (inv_json.items.len > 0) {
         for (inv_json.items, 0..) |it, i| {
             const item_id = @intCast(it.object.get("id").?.integer);
-            const item_name_src = it.object.get("name")?.string;
+            const item_name_src = it.object.get("name").?.string;
             const item_name_dup = try allocator.dupe(u8, item_name_src);
             const item_qty = @intCast(it.object.get("quantity")?.integer);
             try inv_list.addItem(Inventory.Item{ .id = item_id, .name = item_name_dup, .quantity = item_qty });
