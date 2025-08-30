dc3969a97df3d3a860bab48dac045c81c42a8ec2
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-08-30 14:01:08 -0400

    Auto-commit: Sat Aug 30 14:01:08 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -114,7 +114,7 @@ pub const WorldItem = struct {
     ch: u8 = '!', // render as '!' for now
     color: Engine.Color = .{ .r = 255, .g = 255, .b = 0 },
 
-    pub fn init(allocator: std.mem.Allocator) WorldItem {
+    pub fn init(x: i32, y: i32, allocator: std.mem.Allocator) WorldItem {
         return WorldItem{
             .item = Inventory.Item.init(allocator),
             .x = 0,
