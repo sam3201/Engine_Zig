4666858c346280eb5e5c8a4502b6269d8047457b
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 08:58:10 -0400

    Auto-commit: Sun Sep 14 08:58:10 2025

--- src/Client.zig
+++ src/Client.zig
@@ -24,6 +24,8 @@ pub fn disconnectFromServer(stream: *net.Stream) void {
 pub fn sendInput(stream: *net.Stream, input_data: []const u8) !void {
     var write_buf: [1024]u8 = undefined;
     const writer = stream.writer(&write_buf);
+
+    try writer.writeAll(input_data);
 }
 
 pub fn renderGameState(
