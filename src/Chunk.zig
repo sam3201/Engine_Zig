ded4aae2b485808f5b3d302f1547dbaecad88a6a
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 08:54:27 -0400

    Auto-commit: Sun Sep 14 08:54:27 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -142,8 +142,6 @@ pub const Chunk = struct {
         };
 
         var prng = std.Random.DefaultPrng.init(coord.hash());
-        try self.items.append(WorldItem.init(0, 0, allocator)),
-        try self.items.append(WorldItem.init(0, 0, allocator)),
         self.generateTerrain(prng.random());
 
         try self.spawnTestItems();
