3b5df9864ac0243d619ce7c54778c79df0282fc8
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 15:39:28 -0400

    Auto-commit: Sun Sep 14 15:39:28 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -111,7 +111,7 @@ pub const WorldItem = struct {
     item: Inventory.Item,
     x: i32,
     y: i32,
-    ch: u8, 
+    ch: u8,
     color: Engine.Color = .{ .r = 255, .g = 255, .b = 0 },
 
     pub fn init(coord: ChunkCoord, item_type: Inventory.ItemType, quantity: u32, allocator: std.mem.Allocator) WorldItem {
