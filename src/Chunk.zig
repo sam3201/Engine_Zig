404fe50fad7229ea48a9e16e087e0ffc60a818ae
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 15:17:03 -0400

    Auto-commit: Sun Sep 14 15:17:03 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -114,7 +114,7 @@ pub const WorldItem = struct {
     ch: u8 = '!', // render as '!' for now
     color: Engine.Color = .{ .r = 255, .g = 255, .b = 0 },
 
-    pub fn init(id: u32, type: Inventory.ItemType, quantity: u32, x: i32, y: i32, allocator: std.mem.Allocator) WorldItem {
+    pub fn init(id: u32, type: Inventory.ItemType, quantity: u32, allocator: std.mem.Allocator) WorldItem { 
         return WorldItem{
             .item = Inventory.Item.init(id, name, quantity, allocator),
             .x = x,
