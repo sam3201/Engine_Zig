5e19ada418b3bb7301a7c98b0ae989451a2db3e9
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 15:25:37 -0400

    Auto-commit: Sun Sep 14 15:25:37 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -157,7 +157,7 @@ pub const Chunk = struct {
             .x = self.coord.x * CHUNK_WIDTH + 2,
             .y = self.coord.y * CHUNK_HEIGHT + 2,
             .ch = '!',
-            .color = eng.Color{ .r = 200, .g = 0, .b = 200 },
+            .color = Engine.Color{ .r = 200, .g = 0, .b = 200 },
         });
 
         try self.spawnTestItems();
