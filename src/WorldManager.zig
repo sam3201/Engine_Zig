197be08e7ca682fd638bdfa45b96e80e125e6b08
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 16:06:39 -0400

    Auto-commit: Sun Sep 14 16:06:39 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -43,7 +43,7 @@ pub const WorldManager = struct {
         const chunk = try Chunk.Chunk.init(
             .{ .x = 0, .y = 0 },
             0,
-            ' ',
+            Inventory.ItemType.None,
             0,
             start_biome,
             0,
