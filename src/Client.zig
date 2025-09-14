1c9f5de69893577706b170ac8c50e9f908625f01
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 16:16:11 -0400

    Auto-commit: Sun Sep 14 16:16:11 2025

--- src/Client.zig
+++ src/Client.zig
@@ -36,8 +36,8 @@ pub fn renderGameState(
     canvas.clear(' ', eng.Color{ .r = 0, .g = 0, .b = 0 });
 
     while (true) {
-defer alconst line = try reader.readUntilDelimiterAlloc(allocator, '\n', 1024);
-locator.free(line);
+        const line = try reader.readUntilDelimiterAlloc(allocator, '\n', 1024);
+defer allocator.free(line);
 
         if (std.mem.eql(u8, line, "END")) break;
 
