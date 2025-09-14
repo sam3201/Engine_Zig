b572fe88bc17cd5b5453683d8d1dd35e6373b593
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 15:41:50 -0400

    Auto-commit: Sun Sep 14 15:41:50 2025

--- src/Inventory.zig
+++ src/Inventory.zig
@@ -8,7 +8,7 @@ pub const ItemType = enum {
         Axe = 'a',
         Pickaxe = 'p',
     };
-    const Armor = enum(u8) {
+    const Armor = enum {
         Leather = 'l',
         Chain = 'c',
         Iron = 'i',
@@ -26,7 +26,7 @@ pub const Item = struct {
 
     pub fn init(item_type: ItemType, quantity: u32, allocator: std.mem.Allocator) Item {
         return Item{
-            .type = item_type.type,
+            .type = item_type,
             .quantity = quantity,
             .allocator = allocator,
         };
