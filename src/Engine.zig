62e895a77dbe4c423b1ea64426612d98edd31bee
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 09:20:41 -0400

    Auto-commit: Sun Sep 14 09:20:41 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -141,7 +141,6 @@ pub const Canvas = struct {
     }
 
     pub fn flushToTerminal(self: *Canvas) void {
-        const stdout_file = std.io.getStdOut();
         const stdout = std.io.getStdOut().writer();
 
         _ = stdout.write("\x1b[H") catch {};
