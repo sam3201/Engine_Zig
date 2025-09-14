167e488c48c9d3815d0192058dd25d295f0e0ae6
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 16:40:55 -0400

    Auto-commit: Sun Sep 14 16:40:55 2025

--- src/Inventory.zig
+++ src/Inventory.zig
@@ -3,7 +3,7 @@
 const std = @import("std");
 
 pub const Item = struct {
-    pub const ItemType = union(enum) {
+    pub const ItemType = union(struct) {
         None = null,
         Weapon,
         Armor,
