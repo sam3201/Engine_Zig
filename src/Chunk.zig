3995264f8741040c76666310c1217c41bd7fef92
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 16:13:45 -0400

    Auto-commit: Sun Sep 14 16:13:45 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -142,8 +142,6 @@ pub const Chunk = struct {
 
         var items = std.ArrayList(WorldItem).init(allocator);
         try items.append(WorldItem.init(coord, item_type, quantity, allocator));
-        .allocator = allocator;
-        self.items = items;
 
         var prng = std.Random.DefaultPrng.init(coord.hash());
         self.generateTerrain(prng.random());
