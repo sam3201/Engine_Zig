795f136e5e70e3826585ac36e93723d3f61dba1d
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 09:01:58 -0400

    Auto-commit: Sun Sep 14 09:01:58 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -42,8 +42,8 @@ pub const Clock = struct {
 
     pub fn init(fps: f64) Clock {
         return .{
-            .target = std.time.sleep.ns(std.time.ns_per_s) / fps,
-            .last = std.time.nanoTimestamp(),
+            .last = ststd.time.sleep(sleep_ns);
+d.time.nanoTimestamp(),
             .now = std.time.nanoTimestamp(),
         };
     }
