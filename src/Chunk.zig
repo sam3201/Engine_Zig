adf50913db53aeab2f81067882d5e1b529941ebd
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 16:35:53 -0400

    Auto-commit: Sun Sep 14 16:35:53 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -147,7 +147,7 @@ pub const Chunk = struct {
         self.generateTerrain(prng.random());
 
         try self.items.append(WorldItem{
-            .item = Inventory.Item.init(Inventory.ItemType.Potion, 1, allocator),
+            .item = Inventory.Item.init(Inventory.ItemType.Consumable, 1, allocator),
             .x = self.coord.x * CHUNK_WIDTH + 2,
             .y = self.coord.y * CHUNK_HEIGHT + 2,
             .ch = '!',
