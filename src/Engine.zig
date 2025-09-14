d1b0bd87249b3ed86c1d0b6273e090c1aa57a60e
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 15:56:21 -0400

    Auto-commit: Sun Sep 14 15:56:21 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -60,7 +60,7 @@ pub const Clock = struct {
     pub fn sleepUntilNextFrame(self: *Clock) void {
         const target_ns: i128 = @intFromFloat(self.target);
         const sleep_ns: u64 = @intCast(@max(0, target_ns - (self.now - self.last)));
-        sleep_ns: u64 = @intCast(self.target - (self.now - self.last));
+        sleep_ns = @intCast(self.target - (self.now - self.last));
         if (sleep_ns > 0) {
             std.Thread.sleep(sleep_ns);
         }
