6077b9f976aa2fe18eae2a6879eb1881d8a13772
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 15:31:47 -0400

    Auto-commit: Sun Sep 14 15:31:47 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -58,6 +58,7 @@ pub const Clock = struct {
     }
 
     pub fn sleepUntilNextFrame(self: *Clock) void {
+    const now_ns_f64: f64 = @floatFromInt(self.now);
     const sleep_ns: f64 = @floatFromInt(self.target - now_ns_f64); 
     sleep_ns = @intFromFloat(sleep_ns);
     if (sleep_ns > 0) {
