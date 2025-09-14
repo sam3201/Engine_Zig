88bf45f3a8c07b12cb0a2c1fa1c37f884bfbe7ad
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-14 16:17:11 -0400

    Auto-commit: Sun Sep 14 16:17:11 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -40,14 +40,7 @@ pub const WorldManager = struct {
         };
 
         const start_biome = randomBiome();
-        const chunk = try Chunk.Chunk.init(
-            .{ .x = 0, .y = 0 },
-            Inventory.ItemType.None,
-            0,
-            start_biome,
-            0,
-            allocator,
-        );
+const chunk = try Chunk.Chunk.init(coord, biome, difficulty, allocator);
         try world.chunks.put(.{ .x = 0, .y = 0 }, chunk);
 
         try world.updateChunks();
