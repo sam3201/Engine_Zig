c930f3b8cf84aa35203991e3b7e760fa14a80d3f
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-09-14 21:50:13 -0400

    Auto-commit: Sun Sep 14 21:50:13 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -147,14 +147,15 @@ pub const Chunk = struct {
         self.generateTerrain(prng.random());
 
         try self.items.append(allocator, WorldItem{
-        .item = Inventory.Item.initConsumable(.Potion, 1),
-        .x = self.coord.x * CHUNK_WIDTH + 2,
-        .y = self.coord.y * CHUNK_HEIGHT + 2,
-        .color = Engine.Color{ .r = 200, .g = 0, .b = 200 },
-    });
+            .item = Inventory.Item.initConsumable(.Potion, 1),
+            .x = self.coord.x * CHUNK_WIDTH + 2,
+            .y = self.coord.y * CHUNK_HEIGHT + 2,
+            .color = Engine.Color{ .r = 200, .g = 0, .b = 200 },
+        });
 
-    try self.spawnTestItems(allocator);
-    return self;}
+        try self.spawnTestItems(allocator);
+        return self;
+    }
 
     pub fn deinit(self: *Chunk) void {
         self.items.deinit();
