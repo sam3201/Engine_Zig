01faaef45568ffb32d6b2c8a091137d14adc75f7
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-09-14 21:46:41 -0400

    Auto-commit: Sun Sep 14 21:46:41 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -153,13 +153,6 @@ pub const Chunk = struct {
             .color = Engine.Color{ .r = 200, .g = 0, .b = 200 },
         });
 
-        try self.items.append(WorldItem{
-            .item = Inventory.Item.initConsumable(.Potion, 1),
-            .x = self.coord.x * CHUNK_WIDTH + 2,
-            .y = self.coord.y * CHUNK_HEIGHT + 2,
-            .color = Engine.Color{ .r = 200, .g = 0, .b = 200 },
-        });
-
         return self;
     }
 
