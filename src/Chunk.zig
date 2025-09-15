784f159f760cfe56f01ea7cccb462c0d6a8df458
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-09-14 21:45:00 -0400

    Auto-commit: Sun Sep 14 21:45:00 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -146,12 +146,12 @@ pub const Chunk = struct {
         var prng = std.Random.DefaultPrng.init(coord.hash());
         self.generateTerrain(prng.random());
 
-try self.items.append(WorldItem{
-    .item = Inventory.Item.initConsumable(.Potion, 1),
-    .x = self.coord.x * CHUNK_WIDTH + 2,
-    .y = self.coord.y * CHUNK_HEIGHT + 2,
-    .color = Engine.Color{ .r = 200, .g = 0, .b = 200 },
-});
+        try self.items.append(WorldItem{
+            .item = Inventory.Item.initConsumable(.Potion, 1),
+            .x = self.coord.x * CHUNK_WIDTH + 2,
+            .y = self.coord.y * CHUNK_HEIGHT + 2,
+            .color = Engine.Color{ .r = 200, .g = 0, .b = 200 },
+        });
 
         try self.spawnTestItems();
         return self;
