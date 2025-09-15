dd87cd24500467d6e853187bdae5c2fc1e9559c8
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-09-15 10:15:45 -0400

    Auto-commit: Mon Sep 15 10:15:45 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -157,7 +157,7 @@ pub const Chunk = struct {
     }
 
     pub fn deinit(self: *Chunk) void {
-        self.items.deinit(allocator);
+        self.items.deinit(Chunk.allocator);
     }
 
     pub fn addWorldItem(self: *Chunk, wi: WorldItem) !void {
