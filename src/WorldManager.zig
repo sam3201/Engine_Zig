d63eae16730be17960fb60d0914d0f4f6fa77016
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-16 11:48:56 -0400

    Auto-commit: Tue Sep 16 11:48:56 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -203,7 +203,7 @@ pub const WorldManager = struct {
         if (self.chunks.getPtr(chunk_coord)) |chunk| {
             if (chunk.findItemAt(pos.x, pos.y)) |idx| {
                 const wi = chunk.items.items[idx];
-                const item = wi.item; 
+                const item = wi.item;
                 _ = self.player.addItem(item) catch return;
                 chunk.removeItemIndex(idx);
 
