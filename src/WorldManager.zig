b9f6a78e175aad9ea133f2e4be2b080fb7051b9b
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-16 11:43:31 -0400

    Auto-commit: Tue Sep 16 11:43:31 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -228,8 +228,8 @@ pub const WorldManager = struct {
 
             if (chunk.findItemAt(pos.x, pos.y) == null) {
                 chunk.addWorldItem(drop) catch return;
-                self.player.removeItem(item.name, 1);
-                std.debug.print("Dropped {s}\n", .{item.name});
+                self.player.removeItem(item.displayName(), 1);
+                std.debug.print("Dropped {s}\n", .{item.displayName()});
             }
         }
     }
