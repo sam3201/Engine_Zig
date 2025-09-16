6de2004708d231b7ab566fd9fdf700fa0e5bc30b
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-09-16 11:45:40 -0400

    Auto-commit: Tue Sep 16 11:45:40 2025

--- src/main.zig
+++ src/main.zig
@@ -180,7 +180,7 @@ fn ingameMenu(allocator: std.mem.Allocator, engine: *Engine.Engine, player: *Pla
         switch (c) {
             0 => {
                 std.debug.print("Enter new name: ", .{});
-                var buf_arr: [1024]u8 = undefined; 
+                var buf_arr: [1024]u8 = undefined;
                 var stdin_reader = std.fs.stdin().reader(&buf_arr);
                 const line = try stdin_reader.readUntilDelimiterOrEof(&buf_arr, '\n');
                 if (line) |name| {
