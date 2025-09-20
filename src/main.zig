ad614e3cf03e406b6d32801bac3690150f896850
Samuel Dasari <samueldasari@macbookair.mynetworksettings.com>
2025-09-19 20:06:41 -0400

    Auto-commit: Fri Sep 19 20:06:41 2025

--- src/main.zig
+++ src/main.zig
@@ -182,7 +182,7 @@ fn ingameMenu(allocator: std.mem.Allocator, engine: *Engine.Engine, player: *Pla
                 std.debug.print("Enter new name: ", .{});
                 var buf_arr: [1024]u8 = undefined;
                 var stdin_reader = std.fs.File.stdin().reader(&buf_arr);
-                const line = try stdin_reader.readStreaming(&stdin_reader);
+                const line = try stdin_reader.readStreaming(buf_arr[0..]);
                 if (line) |name| {
                     player.*.name = try allocator.dupe(u8, name);
                     // persist player to file (player.json in current dir)
