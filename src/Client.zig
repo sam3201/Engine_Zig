ea18a38647e6cf03ae3b8978801410f0df0a68a8
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-13 20:15:42 -0400

    Auto-commit: Mon Oct 13 20:15:42 2025

--- src/Client.zig
+++ src/Client.zig
@@ -45,7 +45,6 @@ pub fn renderGameState(
     canvas: *eng.Canvas,
 ) !void {
     std.io.bufferedReaderSize(4096, stream.reader());
-
     const reader = std.io.bufferedReaderSize(4096, stream.reader());
 
     canvas.clear(' ', eng.Color{ .r = 0, .g = 0, .b = 0 });
