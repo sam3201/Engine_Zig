2fc502b7b10fe82bb8cc43345b62b0c0d6aeb6fd
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-13 20:10:08 -0400

    Auto-commit: Mon Oct 13 20:10:08 2025

--- src/Player.zig
+++ src/Player.zig
@@ -2,7 +2,7 @@
 const std = @import("std");
 const Inventory = @import("Inventory.zig");
 const Engine = @import("Engine.zig");
-const Entity = @import("Entity.zig");
+const Entity = @import("Entity.zig").Entity;
 
 pub const Player = struct {
     entity: Entity,
