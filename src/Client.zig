0c816a501eb9f07fa7464c6fe4489b1d56eaa7b0
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-13 20:14:12 -0400

    Auto-commit: Mon Oct 13 20:14:12 2025

--- src/Client.zig
+++ src/Client.zig
@@ -44,7 +44,8 @@ pub fn renderGameState(
     allocator: std.mem.Allocator,
     canvas: *eng.Canvas,
 ) !void {
-    var buffered_reader = std.io.bufferedReader(stream.reader());
+    var buffered_reader = std.io.bufferedReader(std.io.fixedBufferStream(&buffer).reader());
+
     const reader = buffered_reader.reader();
 
     canvas.clear(' ', eng.Color{ .r = 0, .g = 0, .b = 0 });
