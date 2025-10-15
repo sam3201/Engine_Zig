2544dba42482a1c0c4deb4137d83650921a2cec1
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 19:42:14 -0400

    Auto-commit: Wed Oct 15 19:42:14 2025

--- src/Server.zig
+++ src/Server.zig
@@ -190,7 +190,7 @@ pub const GameServer = struct {
 
             self.mutex.lock();
             if (self.players[id]) |*player_info| {
-+            const action = player_info.player.processInput(trimmed_input[0]);
+const action = player_info.player.processInput(trimmed_input[0]);
             // FIX 3: Now properly handles the error return from handlePlayerAction
             try self.world_manager.handlePlayerAction(action);
         }
