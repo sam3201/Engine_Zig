c115b9d95d2c7a1c0ddeb0b0504b46076d9f6d81
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 19:39:41 -0400

    Auto-commit: Wed Oct 15 19:39:41 2025

--- src/Server.zig
+++ src/Server.zig
@@ -185,6 +185,7 @@ pub const GameServer = struct {
                     break;
                 };
 
+            if (line == null) break;
             self.mutex.lock();
             if (self.players[id]) |*player_info| {
                 const action = player_info.player.processInput(input[0]);
