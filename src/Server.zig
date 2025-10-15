4c15cfc4957afe6d50fc0de0b3febfeb0404b5fb
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 19:37:22 -0400

    Auto-commit: Wed Oct 15 19:37:22 2025

--- src/Server.zig
+++ src/Server.zig
@@ -137,8 +137,9 @@ pub const GameServer = struct {
         var reader_buffer = std.io.bufferedReader(connection.stream.reader(), &buffer);
         var reader = reader_buffer.reader();
 
-        var writer_buffer = std.io.Writer(connection.stream.writer());
-        var writer = writer_buffer.writer();
+        var writer_buffer: [1024]u8 = undefined;
+        var writer_buffer_stream = std.io.fixedBufferStream(&writer_buffer);
+        var writer = writer_buffer_stream.writer();
 
         // Create new player
         self.mutex.lock();
