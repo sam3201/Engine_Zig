575d62769a7c0b71de4f38738eb193e191e3d91c
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 19:40:41 -0400

    Auto-commit: Wed Oct 15 19:40:41 2025

--- src/Server.zig
+++ src/Server.zig
@@ -187,6 +187,7 @@ pub const GameServer = struct {
 
             if (line == null) break;
             const input = std.mem.trim(u8, line.?, "\n\r");
+
             self.mutex.lock();
             if (self.players[id]) |*player_info| {
                 const action = player_info.player.processInput(input[0]);
