478c2f975b43a7b0b3613c6c2ec0a1b7b1f845c2
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 19:59:23 -0400

    Auto-commit: Wed Oct 15 19:59:23 2025

--- src/Client.zig
+++ src/Client.zig
@@ -11,6 +11,7 @@ var g_read_buf: [4096]u8 = undefined;
 var g_write_buf: [1024]u8 = undefined;
 var g_allocator: ?std.mem.Allocator = null;
 var g_reader: ?std.io.Reader = null;
+
 fn readLineAlloc(allocator: std.mem.Allocator, reader: std.io.Reader, max_len: usize) ![]u8 {
     var line_writer = std.io.Writer.Allocating.init(allocator);
     defer line_writer.deinit();
