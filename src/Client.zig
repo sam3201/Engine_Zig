3884c447adcd2f4850f48162944352580c23e2f2
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 23:40:13 -0400

    Auto-commit: Wed Oct 15 23:40:13 2025

--- src/Client.zig
+++ src/Client.zig
@@ -20,7 +20,9 @@ var g_allocator: ?std.mem.Allocator = null;
 pub fn connectToServer() !void {
     const address = try net.Address.parseIp("127.0.0.1", 42069);
     g_stream = try net.tcpConnectToAddress(address);
-    g_connection = try g_stream.accept();
+
+    g_stream_reader = g_stream.?.reader();
+    g_allocator = std.heap.page_allocator;
 
     g_stream_writer = g_stream.?.writer(&g_write_buff);
 
