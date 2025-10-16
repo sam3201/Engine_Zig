1bd91c719918eb1e7ef7a04322adc9f87f276a08
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 22:03:06 -0400

    Auto-commit: Wed Oct 15 22:03:06 2025

--- src/Server.zig
+++ src/Server.zig
@@ -136,7 +136,7 @@ pub const GameServer = struct {
 
         const reader = connection.stream.reader(&read_buffer);
         //print typeof(reader) during debug
-        std.debug.print("reader type: {}\n", .{@typeInfo(@TypeOf(reader))});
+        @compileError(std.debug.print("reader type: {}\n", .{@typeInfo(@TypeOf(reader))});
 
         const writer = connection.stream.writer(&write_buffer);
 
