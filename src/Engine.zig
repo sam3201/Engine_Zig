8093c4caae57b9cd0606fcb7b3f56046574dc420
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-16 15:26:52 -0400

    Auto-commit: Thu Oct 16 15:26:52 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -208,8 +208,9 @@ const written: usize = std.posix.write(std.posix.STDOUT_FILENO, chunk) catch |er
         };
         
         total_written += written;
+        }
     }
-}} 
+} 
 
 pub fn addRenderable(self: *Canvas, r: Renderable) !void {
         try self.scene.append(r);
