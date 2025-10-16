478d51d0f53ded865f2c0179853cdb9ff15ee3e2
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-16 14:56:52 -0400

    Auto-commit: Thu Oct 16 14:56:52 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -179,7 +179,7 @@ pub const Canvas = struct {
 
         try writer.writeAll("\x1b[0m");
 
-        try writer.writeAll(self.render_buffer.items);
+        try writer.print(self.render_buffer.items);
 
     }
 
