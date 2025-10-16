3f37ac3f436cd7d550c7ef8ebed5c2c8824cff43
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 21:51:20 -0400

    Auto-commit: Wed Oct 15 21:51:20 2025

--- src/Client.zig
+++ src/Client.zig
@@ -134,8 +134,8 @@ pub fn main() !void {
     var stream = try connectToServer();
     g_stream = &stream;
     g_allocator = allocator;
-    var *reader_struct = stream.reader(&g_read_buf);
-    g_reader = reader_struct;
+    const reader_struct = *stream.reader(&g_read_buf);
+    g_reader = &reader_struct;
 
     defer disconnectFromServer(&stream);
 
