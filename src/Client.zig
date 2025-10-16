e50daea5d440653723ff1671b3c800321d158d88
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 23:37:26 -0400

    Auto-commit: Wed Oct 15 23:37:26 2025

--- src/Client.zig
+++ src/Client.zig
@@ -5,7 +5,7 @@ const net = std.net;
 const eng = @import("Engine.zig");
 const Chunk = @import("Chunk.zig");
 
-var g_connection: ?net.Stream = null;
+var g_connection: ?net.Connection = null;
 
 var g_stream_reader: ?net.Stream.Reader = null;
 const read_buff_max = 4096;
