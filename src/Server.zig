6e0fd8ea8b8e071daa379aa8e27ce2991d8288e2
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-16 00:16:15 -0400

    Auto-commit: Thu Oct 16 00:16:15 2025

--- src/Server.zig
+++ src/Server.zig
@@ -24,7 +24,6 @@ pub const GameServer = struct {
         player: Player,
         client_id: u32,
         connection: net.Server.Connection,
-        is_host: bool,
     };
 
     pub fn init(allocator: std.mem.Allocator) !GameServer {
