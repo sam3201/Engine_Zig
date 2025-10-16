11382581ffdf28e3d7c08182de5db35b19a64c58
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 20:10:34 -0400

    Auto-commit: Wed Oct 15 20:10:34 2025

--- src/Server.zig
+++ src/Server.zig
@@ -135,7 +135,6 @@ pub const GameServer = struct {
         var buffer: [1024]u8 = undefined;
         var reader = std.io.bufferedReader(connection.stream.reader(), &buffer).reader();
 
-        // I/O FIX 1: Use a buffered writer for efficiency
         var writer_buffer = std.io.bufferedWriter(connection.stream.writer());
         var writer = writer_buffer.writer();
 
