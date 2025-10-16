de16df7be5e8f3ed43a1b9f384043b551ce5857c
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-16 01:47:41 -0400

    Auto-commit: Thu Oct 16 01:47:41 2025

--- src/Server.zig
+++ src/Server.zig
@@ -66,7 +66,7 @@ pub const GameServer = struct {
         var key_iterator = world_manager.chunks.keyIterator(); 
         while (key_iterator.next()) |coord| { 
             if (world_manager.chunks.getPtr(coord.*)) |chunk| { 
-                [cite_start]chunk.generate(); [cite: 305]
+                [cite_start]chunk.generate(); 
             }
         }
 
