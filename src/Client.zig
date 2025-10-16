c30e3f2f7eb1f33b8543ae0b1928c5c79c8d8d27
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 23:40:25 -0400

    Auto-commit: Wed Oct 15 23:40:25 2025

--- src/Client.zig
+++ src/Client.zig
@@ -22,6 +22,7 @@ pub fn connectToServer() !void {
     g_stream = try net.tcpConnectToAddress(address);
     g_connection = try g_stream.?.accept();
 
+
     g_stream_writer = g_stream.?.writer(&g_write_buff);
 
     std.debug.print("Connected to server\n", .{});
