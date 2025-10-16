def11b6b365dc5de0d1882b5ff23c43741e83cb1
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-16 15:07:42 -0400

    Auto-commit: Thu Oct 16 15:07:42 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -180,7 +180,9 @@ pub const Canvas = struct {
         try writer.writeAll("\x1b[0m");
 
         //try writer.writeAll(self.render_buffer.items);
-        try writer.flush(); 
+        var stdout_writer = std.fs.File.stdout().writer(self.render_buffer).interface;
+        try stdout_writer.writeAll(self.render_buffer.items);
+        try stdout_writer.flush(); 
     }
 
     pub fn addRenderable(self: *Canvas, r: Renderable) !void {
