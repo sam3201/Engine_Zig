cb91fc8caa8d03fb1145a1d46ad847ace7cb7faf
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-16 01:14:39 -0400

    Auto-commit: Thu Oct 16 01:14:39 2025

--- src/Server.zig
+++ src/Server.zig
@@ -9,6 +9,9 @@ const Engine = @import("Engine.zig");
 const Chunk = @import("Chunk.zig");
 
 var g_server: ?*GameServer = null;
+var g_read_buffer: [4096]u8 = undefined;
+var g_write_buffer: [4096]u8 = undefined;
+
 const MAX_PLAYERS = 64;
 
 pub const GameServer = struct {
