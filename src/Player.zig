bb93703c4953a4f7304fdb43e912842601fac298
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 20:49:32 -0400

    Auto-commit: Wed Oct 15 20:49:32 2025

--- src/Player.zig
+++ src/Player.zig
@@ -149,7 +149,7 @@ pub const Player = struct {
         }
     }
 
-    pub fn load(self: *Player, name: []const u8) !void {
+    pub fn load(name: []const u8) !void {
         const fs = std.fs.cwd();
         var file = try fs.openFile(name, .{}) catch |err| {
             std.log.err("{err}: Player Doesn't Exist on Computer: {s}", .{ err, name });
