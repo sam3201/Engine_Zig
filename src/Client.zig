bb60bf2e4d480be84e9c580d5ee96572ada66070
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 20:09:46 -0400

    Auto-commit: Wed Oct 15 20:09:46 2025

--- src/Client.zig
+++ src/Client.zig
@@ -135,8 +135,7 @@ pub fn main() !void {
     var stream = try connectToServer();
     g_stream = &stream;
     g_allocator = allocator;
-    g_reader = std.io.bufferedReader(stream.reader(), &g_read_buf);
-
+g_reader = std.io.bufferedReader(stream.reader(), &g_read_buf).reader();
     defer disconnectFromServer(&stream);
 
     engine.canvas.setUpdateFn(update);
