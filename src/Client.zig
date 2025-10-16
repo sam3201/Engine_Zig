ac2daa9635aaad6c382377f03e31032466e664d0
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 21:20:15 -0400

    Auto-commit: Wed Oct 15 21:20:15 2025

--- src/Client.zig
+++ src/Client.zig
@@ -42,7 +42,7 @@ pub fn sendInput(stream: *net.Stream, input_data: []const u8) !void {
 
 pub fn renderGameState(
     allocator: std.mem.Allocator,
-    reader: std.io.Reader,
+    reader: net.Stream.Reader, 
     canvas: *eng.Canvas,
 ) !void {
     canvas.clear(' ', eng.Color{ .r = 0, .g = 0, .b = 0 });
