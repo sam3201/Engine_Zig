6307ca1b7ce457a5ee4d261d6dd527b0e15fee00
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 22:14:30 -0400

    Auto-commit: Wed Oct 15 22:14:30 2025

--- src/Client.zig
+++ src/Client.zig
@@ -36,7 +36,7 @@ pub fn renderGameState(
     canvas.clear(' ', eng.Color{ .r = 0, .g = 0, .b = 0 });
 
     while (true) {
-        const len = try reader.read(&g_read_buf);
+        const buff_len = try reader.readSliceAll(&g_read_buf);
         const read = try reader.readAlloc(
             &g_reader,
             &allocator,
