00b3a1b5cf258dc3d7b12316b95c699b3993b808
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 20:32:16 -0400

    Auto-commit: Wed Oct 15 20:32:16 2025

--- src/Server.zig
+++ src/Server.zig
@@ -132,7 +132,9 @@ pub const GameServer = struct {
     fn handleClient(self: *GameServer, connection: net.Server.Connection) !void {
         defer connection.stream.close();
 
-        var buffer: [1024]u8 = undefined;
+        var read_buffer: [1024]u8 = undefined;
+        var write_buffer: [1024]u8 = undefined;
+
         var reader = connection.stream.reader(&buffer);
 
         var write_buffer: [1024]u8 = undefined;
