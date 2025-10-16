55a458cb2542c3fe4bb55a2a7b963378be161648
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 20:43:54 -0400

    Auto-commit: Wed Oct 15 20:43:54 2025

--- src/Player.zig
+++ src/Player.zig
@@ -151,7 +151,7 @@ pub const Player = struct {
 
     pub fn load(name: []const u8, path: []const u8) !void {
         const fs = std.fs.cwd();
-        var file = fs.openFile(path, .{}) catch |err| {
+        var file = try fs.openFile(path, .{}) catch |err| {
             std.log.err("Failed to open file: {s}", .{path});
             return err;
         };
