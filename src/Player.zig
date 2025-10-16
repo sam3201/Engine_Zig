ef1b2165b47b8b660182f66813592afee303b66a
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 20:57:44 -0400

    Auto-commit: Wed Oct 15 20:57:44 2025

--- src/Player.zig
+++ src/Player.zig
@@ -145,7 +145,7 @@ pub const Player = struct {
         try w.print("y: {d}\n", .{self.entity.y});
         try w.writeAll("inventory:\n");
         for (self.inventory.items.items) |it| {
-            try it.saveToFile(w);
+            try w.print("{s}\n", .{it.name});
         }
     }
 
