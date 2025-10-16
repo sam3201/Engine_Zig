3075e0cc6b8bbfd0562183f25f3eeaa5758ea1d2
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-16 01:09:23 -0400

    Auto-commit: Thu Oct 16 01:09:23 2025

--- src/Server.zig
+++ src/Server.zig
@@ -72,7 +72,12 @@ pub const GameServer = struct {
 
         // Add host player
         self.mutex.lock();
-        self.players[self.player_count] = .{ .player = self.world_manager.player, .client_id = @intCast(u32, self.player_count) };
+        self.players[self.player_count] = .{
+            
+            .player = self.world_manager.host_player,
+            .client_id = 0,
+            .connection = undefined,
+        }; 
         self.player_count += 1;
         self.mutex.unlock();
 
