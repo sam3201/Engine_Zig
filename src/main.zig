ad472343d98c0082771c84c5b50f2fe3bb0dcd6f
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-16 19:44:31 -0400

    Auto-commit: Thu Oct 16 19:44:30 2025

--- src/main.zig
+++ src/main.zig
@@ -10,7 +10,7 @@ pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
     const WIDTH = 84;
-    const HEIGHT = 50;
+    const HEIGHT = 80;
     const FPS = 60; 
 
     var engine = try Engine.Engine.init(
