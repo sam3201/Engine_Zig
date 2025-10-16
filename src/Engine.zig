2a9768f83bcb320e676347751697345623b05e91
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-16 15:04:00 -0400

    Auto-commit: Thu Oct 16 15:04:00 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -180,7 +180,7 @@ pub const Canvas = struct {
         try writer.writeAll("\x1b[0m");
 
         //try writer.writeAll(self.render_buffer.items);
-        var stdout_writer = std.fs.File.stdout.writer();
+        var stdout_writer = std.fs.File.stdout().writer(self.render_buffer.writer(self.allocator));
         try stdout_writer.writeAll(self.render_buffer.items);
         try stdout_writer.flush(); 
     }
