ed347250f8c1c35148ec408591143fb15038c562
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 21:01:44 -0400

    Auto-commit: Wed Oct 15 21:01:44 2025

--- src/Server.zig
+++ src/Server.zig
@@ -136,7 +136,7 @@ pub const GameServer = struct {
         var write_buffer: [1024]u8 = undefined;
 
         var reader_struct = connection.stream.reader(&read_buffer);
-        const reader = &reader_struct.interface;
+        const reader = reader_struct.interface;
 
         const writer = connection.stream.writer(&write_buffer).interface;
 
