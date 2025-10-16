d1e2cc740aeb8675267f6c506694ee51f25ab72c
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-16 19:01:32 -0400

    Auto-commit: Thu Oct 16 19:01:32 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -195,7 +195,7 @@ const bytes_to_write = self.render_buffer.items;
     //const flags = try std.posix.fcntl(fd, std.posix.F.GETFL, 0);
     //const O_NONBLOCK: u32 = 0x0004;
     //_ = try std.posix.fcntl(fd, std.posix.F.SETFL, flags & ~O_NONBLOCK);
-    writer.print("{any}", .{bytes_to_write}) catch unreachable; // (bytes_to_write);
+    writer.print("{any}", .{bytes_to_write}) catch {}; // (bytes_to_write);
 
 }
     
