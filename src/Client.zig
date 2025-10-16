29c075a6347f9668f933e21a16c506f618b32a4a
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-15 23:15:52 -0400

    Auto-commit: Wed Oct 15 23:15:52 2025

--- src/Client.zig
+++ src/Client.zig
@@ -19,7 +19,7 @@ pub fn connectToServer() !net.Stream {
     const address = try net.Address.parseIp("127.0.0.1", 42069);
     const stream = try net.tcpConnectToAddress(address);
     g_stream_reader = stream.reader();
-    g_reader = &g_stream_reader.interface;
+    g_reader = &g_stream_reader.?.reader();
     std.debug.print("Connected to server\n", .{});
     return stream;
 }
