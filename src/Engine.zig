258fa1e913166297c76c1cc5284297fe538ce948
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-16 19:08:31 -0400

    Auto-commit: Thu Oct 16 19:08:31 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -335,8 +335,7 @@ pub const TerminalGuard = struct {
     tg.orig_flags = flags;
 
         const nonblock_bits: usize = 0x0004;
-        _ = try std.posix.fcntl(std.posix.STDIN_FILENO, std.posix.F.SETFL, flags | nonblock_bits);
-
+_ = try std.posix.fcntl(std.posix.STDIN_FILENO, std.posix.F.SETFL, flags | nonblock_bits);
         _ = try std.posix.write(std.posix.STDOUT_FILENO, "\x1b[?25l");
 
         return tg;
