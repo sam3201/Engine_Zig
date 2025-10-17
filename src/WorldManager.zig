a7a2920a16440671fa799285705e411e7ce24d70
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-17 18:03:56 -0400

    Auto-commit: Fri Oct 17 18:03:56 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -257,7 +257,7 @@ pub const WorldManager = struct {
 
     fn playerSelectInventorySlot(self: *WorldManager, slot: u8) void {
         if (self.player.inventory.getItem(slot)) |item| {
-            self.player.inventory.slected_item = idx;
+            self.player.inventory.slected_item = slot;
         }
         
     }
