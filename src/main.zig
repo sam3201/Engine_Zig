f1ab1658bb17be0e8a47f409239f1a6820828cb8
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-17 18:19:38 -0400

    Auto-commit: Fri Oct 17 18:19:38 2025

--- src/main.zig
+++ src/main.zig
@@ -57,7 +57,7 @@ pub fn main() !void {
             0 => std.debug.print("Starting game...\n", .{}),
             1 => try optionsMenu(&engine),
             2 => return,
-            3 => break,
+            3 => return,
             else => {},
         }
     }
