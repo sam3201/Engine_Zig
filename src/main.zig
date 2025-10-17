43856afc1ec24d8bf71283f488e9069a8f94197e
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-17 19:01:21 -0400

    Auto-commit: Fri Oct 17 19:01:21 2025

--- src/main.zig
+++ src/main.zig
@@ -63,7 +63,7 @@ pub fn main() !void {
 
     // ───────────── Game Start ─────────────
     _ = std.posix.write(std.posix.STDOUT_FILENO, "\x1b[2J\x1b[H") catch {};
-    const camera = Engine.Camera.init(WIDTH, HEIGHT);
+    var camera = Engine.Camera.init(WIDTH, HEIGHT);
 
     var game_engine = try Engine.Engine.init(
         allocator,
