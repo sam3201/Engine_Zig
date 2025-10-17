c5987472d6fdc1f77209eecad30f98175826f52c
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-17 17:48:24 -0400

    Auto-commit: Fri Oct 17 17:48:24 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -272,6 +272,8 @@ pub const Engine = struct {
         var term = try TerminalGuard.init();
         defer term.deinit();
 
+        _ = std.posix.write(std.posix.STDOUT_FILENO, "\x1b[2J\x1b[H") catch {};
+
         while (self.running) {
             self.clock.tick();
 
