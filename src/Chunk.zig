f7b229f3414ebc2aaff82dc46d445ab900288abe
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-17 21:13:00 -0400

    Auto-commit: Fri Oct 17 21:13:00 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -5,8 +5,8 @@ const Engine = @import("Engine.zig");
 const Inventory = @import("Inventory.zig");
 const WorldManager = @import("WorldManager.zig");
 
-pub const CHUNK_WIDTH: i32 = 80;
-pub const CHUNK_HEIGHT: i32 = 24;
+pub const CHUNK_WIDTH: i32 = 80*80;
+pub const CHUNK_HEIGHT: i32 = 24*24;
 
 pub const TileType = enum {
     Empty,
