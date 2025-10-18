ad444fe423fc0df9f29e41e44d7713c363bf6202
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-17 20:48:32 -0400

    Auto-commit: Fri Oct 17 20:48:32 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -347,7 +347,7 @@ pub const Engine = struct {
             self.clock.sleepUntilNextFrame();
         }
 
-        try std.posix.write(std.posix.STDOUT_FILENO, "\x1b[?25h\x1b[0m\n") catch {}; 
+        _ = std.posix.write(std.posix.STDOUT_FILENO, "\x1b[?25h\x1b[0m\n") catch {}; 
     }   
     
 };
