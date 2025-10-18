989f566f517aa6aef810b01220f73f3f296898cf
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-17 20:37:30 -0400

    Auto-commit: Fri Oct 17 20:37:30 2025

--- src/main.zig
+++ src/main.zig
@@ -9,7 +9,7 @@ const Menu = @import("Menu.zig").Menu;
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
-    const WIDTH = 80;
+    const WIDTH = 150;
     const HEIGHT = 50;
     
     const FPS = 60; 
