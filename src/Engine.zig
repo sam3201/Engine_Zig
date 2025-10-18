b0115ce72e65be628d2c191f1ee72723726aff91
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-17 20:42:58 -0400

    Auto-commit: Fri Oct 17 20:42:58 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -179,8 +179,6 @@ pub fn clear(self: *Canvas, ch: u8, color: Color) void {
         }
     }
 
-    //var total_written: usize = 0;
-    const retries: usize = 0;
     while (total_written < bytes_to_write.len) { //and retries < 10) {
     try std.posix.write(std.posix.STDOUT_FILENO, self.render_buffer.items); 
         // if (err == error.WouldBlock) {
