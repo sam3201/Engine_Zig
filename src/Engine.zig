076f4457b062297e936fafb72b24853e13a6e523
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-17 20:41:15 -0400

    Auto-commit: Fri Oct 17 20:41:15 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -370,7 +370,7 @@ pub const Engine = struct {
         }
 
         try std.posix.write(std.posix.STDOUT_FILENO, "\x1b[?25h\x1b[0m\n"); 
-};
+}
 
 pub const TerminalGuard = struct {
     orig: std.posix.termios,
