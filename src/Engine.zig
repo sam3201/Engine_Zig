5945c8354a5ce3171fd9654472783db2c2a1075d
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-17 20:34:35 -0400

    Auto-commit: Fri Oct 17 20:34:35 2025

--- src/Engine.zig
+++ src/Engine.zig
@@ -191,7 +191,7 @@ pub fn clear(self: *Canvas, ch: u8, color: Color) void {
            // }
             // return err;
         };
-        if (n == 0) break;
+        // if (n == 0) break;
         total_written += n;
         retries = 0; 
     }
