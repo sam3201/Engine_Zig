223b2a827ccdcd84d45015e8e3578ae7c6c34d21
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-19 19:10:32 -0400

    Auto-commit: Sun Oct 19 19:10:32 2025

--- src/Server.zig
+++ src/Server.zig
@@ -26,7 +26,7 @@ pub const GameServer = struct {
         var dummy_canvas = try Engine.Canvas.init(allocator, 1, 1);
         const host_player = try Player.Player.createWASDPlayer("host", allocator, 10, 10);
 
-        var world_manager = try WorldManager.WorldManager.init(Chunk.ChunkCoord{ .x = 0, .y = 0 }, 0, allocator, &dummy_canvas, host_player);
+        const world_manager = try WorldManager.WorldManager.init(Chunk.ChunkCoord{ .x = 0, .y = 0 }, 0, allocator, &dummy_canvas, host_player);
 
         const address = try net.Address.parseIp("127.0.0.1", 42069);
         const listener_socket = try posix.socket(address.any.family, posix.SOCK.STREAM, 0);
