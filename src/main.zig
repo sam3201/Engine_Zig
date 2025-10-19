c33c2ae116db63024e29511a1e9916254a171fe2
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-19 19:55:36 -0400

    Auto-commit: Sun Oct 19 19:55:36 2025

--- src/main.zig
+++ src/main.zig
@@ -10,6 +10,8 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
+    const GameServer = @import("Server.zig").GameServer;
+
     if (args.len >= 2 and std.mem.eql(u8, args[1], "server")) {
         var server = try Server.GameServer.init(allocator);
         defer server.deinit();
