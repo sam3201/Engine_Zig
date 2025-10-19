53c7c53b4b0cb16ec98da38e1def3d8ffaadb68c
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-18 21:43:56 -0400

    Auto-commit: Sat Oct 18 21:43:55 2025

--- src/Server.zig
+++ src/Server.zig
@@ -17,7 +17,7 @@ const GameState = struct {
 
 pub const GameServer = struct {
     allocator: std.mem.Allocator,
-    listener: net.StreamServer,
+    listener: std.posix.socket_t,
     clients: std.ArrayList(net.StreamServer.Connection),
     world_state: GameState,
     running: bool,
