cc05f605267329546fe9950ff432f776556fa43c
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-18 22:24:06 -0400

    Auto-commit: Sat Oct 18 22:24:06 2025

--- src/Client.zig
+++ src/Client.zig
@@ -77,7 +77,6 @@ pub fn updateAndRender(canvas: *eng.Canvas) void {
         parseState(g_read_buffer[0..bytes_read]) catch |err| {
             std.debug.print("Parse error: {any}\n", .{err});
         };
-    }
     };
 
     // Render the current known state
