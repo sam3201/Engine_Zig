19980bc649a7630549528760749048f1dd7eac07
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-19 22:00:31 -0400

    Auto-commit: Sun Oct 19 22:00:31 2025

--- src/main.zig
+++ src/main.zig
@@ -18,7 +18,7 @@ const c = @cImport({
     @cInclude("unistd.h");
 });
 
-pub fn getTerminalSize() !u8[] {
+pub fn getTerminalSize() ![]u8 {
     var size = c.winsize{};
     if (c.ioctl(c.STDOUT_FILENO, c.TIOCGWINSZ, &size) == 0) {
         std.debug.print("Terminal size: {d} rows, {d} columns\n", .{size.ws_row, size.ws_col});
