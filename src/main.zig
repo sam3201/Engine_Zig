52a1706f6fd0a792cfc89fc2eb47a7653889c41b
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-20 15:07:02 -0400

    Auto-commit: Mon Oct 20 15:07:02 2025

--- src/main.zig
+++ src/main.zig
@@ -44,7 +44,7 @@ pub fn mainMenu(engine: *Engine.Engine) !u8 {
                     break;
                 }
 
-                if (byte) |ch| {
+                if (byte != null) |ch| {
                     menu.update(ch);
                 }
                 menu.draw(&engine.canvas);
