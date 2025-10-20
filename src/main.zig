0fac985cff866d4efd795ce9c90fbfd76281edba
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-20 11:49:55 -0400

    Auto-commit: Mon Oct 20 11:49:55 2025

--- src/main.zig
+++ src/main.zig
@@ -22,7 +22,7 @@ pub fn getTerminalSize() ![]u8 {
     return try std.mem.toBytes(result);
 }
 
-pub fn mainMenu(engine: *Engine.Engine) !u8 {
+pub fn drawMainMenu(engine: *Engine.Engine) !u8 {
     const items = [_][]const u8{
         "SinglePlayer",
         "Host Game",
