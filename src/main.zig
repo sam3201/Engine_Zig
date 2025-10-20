4eedf86e215959483b5d01c43980983bb582cb5b
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-20 11:46:30 -0400

    Auto-commit: Mon Oct 20 11:46:30 2025

--- src/main.zig
+++ src/main.zig
@@ -84,7 +84,7 @@ pub fn main() !void {
 
 fn runSingleplayer(allocator: std.mem.Allocator, engine: *Engine.Engine, player: *Player.Player) !void {
     std.debug.print("\nStarting singleplayer...\n", .{});
-    const world_manager = try WorldManager.WorldManager.init(Chunk.ChunkCoord{ .x = 0, .y = 0 }, 0, allocator, &engine.canvas, );
+    const world_manager = try WorldManager.WorldManager.init(Chunk.ChunkCoord{ .x = 0, .y = 0 }, 0, allocator, &engine.canvas, player);
 
     
 }
