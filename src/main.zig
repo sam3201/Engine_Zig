b8aa8a87a30e4af97da846ab37f97b4f4f3a800a
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-21 04:15:33 -0400

    Auto-commit: Tue Oct 21 04:15:33 2025

--- src/main.zig
+++ src/main.zig
@@ -68,8 +68,6 @@ pub fn mainMenu(engine: *Engine.Engine) !u8 {
         }
 
         _ = std.posix.write(std.posix.STDOUT_FILENO, "\x1b[?25h\x1b[0m\n") catch {}; 
-
-        return selection; 
 }
  
 pub fn main() !void {
