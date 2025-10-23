e9c4c086d3924a98b06783cea94fb01360e2738f
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-23 19:10:25 -0400

    Auto-commit: Thu Oct 23 19:10:25 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -485,7 +485,7 @@ pub fn projectTo3D(self: *WorldManager, canvas3D: *Engine3D.Canvas3D, cam3d: *En
         .Lava => .{ .h = 1, .ch = '=', .color = Engine3D.Color3D.init(255,80,0) },
         .Wall => .{ .h = 2, .ch = '#', .color = Engine3D.Color3D.init(100,60,0) },
     };
-};
+}
 
 
     canvas3D.clear(' ', Engine3D.Color3D.init(0,0,0));
