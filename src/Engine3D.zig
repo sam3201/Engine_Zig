aef024813c40885ee8b4fcbaf375258296d3c515
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-23 18:46:36 -0400

    Auto-commit: Thu Oct 23 18:46:36 2025

--- src/Engine3D.zig
+++ src/Engine3D.zig
@@ -99,7 +99,7 @@ pub const Canvas3D = struct {
 
     updateFn: ?UpdateFn = null,
 
-    pub fn init(allocator: std.mem.Allocator, width: usize, height: usize) !Canvas {
+    pub fn init(allocator: std.mem.Allocator, width: usize, height: usize) !Canvas3D {
     const render_buffer_capacity = (width * height) + (width * height * 20) + height;
 
         var canvas = Canvas{
