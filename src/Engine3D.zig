809f5e7c3d2f4f7101aa2396dd732aceadf9d713
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-23 18:46:57 -0400

    Auto-commit: Thu Oct 23 18:46:57 2025

--- src/Engine3D.zig
+++ src/Engine3D.zig
@@ -112,7 +112,7 @@ pub const Canvas3D = struct {
             .updateFn = null,
             .render_buffer = try std.ArrayList(u8).initCapacity(allocator, render_buffer_capacity),
         };
-        canvas.updateFn = Canvas.update;
+        canvas3D.updateFn = Canvas.update;
         return canvas;
     }
 
