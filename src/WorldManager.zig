4e2bef480219be3b3163e2707f7aea661043d0eb
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-24 11:34:37 -0400

    Auto-commit: Fri Oct 24 11:34:37 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -472,7 +472,7 @@ pub fn projectTo3D(self: *WorldManager, canvas3D: *Engine3D.Canvas3D, cam3d: *En
     const screen_h: i32 = @intCast(canvas3D.height);
 
     const tile_to_height_and_char = fn (t: Chunk.TileType) struct {h: i32, ch: u8, color: Engine3D.Color3D} {
-    switch (t) {
+    return switch (t) {
         .Empty => .{ .h = 0, .ch = '.', .color = Engine3D.Color3D.init(64,64,64) },
         .Grass => .{ .h = 1, .ch = ',', .color = Engine3D.Color3D.init(20,120,20) },
         .Tree  => .{ .h = 3, .ch = 'T', .color = Engine3D.Color3D.init(0,100,0) },
