9c69696cf20294f07ccb8dfc2b055b70307d1489
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-25 20:32:11 -0400

    Auto-commit: Sat Oct 25 20:32:11 2025

--- src/Particle.zig
+++ src/Particle.zig
@@ -4,7 +4,7 @@ const std = @import("std");
 const Engine3D = @import("Engine3D.zig");
 const Chunk = @import("Chunk.zig");
 
-pub const MAX_PARTICLES: [Chunk.WIDTH * Chunk.HEIGHT]Particle{}; 
+pub const MAX_PARTICLES: [Chunk.WIDTH * Chunk.HEIGHT]Particle; 
 
 pub const Particle = struct {
     x: f32,
