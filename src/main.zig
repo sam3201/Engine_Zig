e80f381efef6358ae38872cd99839d32fa03a5da
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-25 21:07:46 -0400

    Auto-commit: Sat Oct 25 21:07:46 2025

--- src/main.zig
+++ src/main.zig
@@ -135,7 +135,7 @@ fn runSingleplayer3D(allocator: std.mem.Allocator, engine: *Engine.Engine) !void
         }
 
         const mouse = try Engine.readMouse() catch |err| @panic(@errorName(err));
-        if (mouse.delta_x != 0 or mouse.delta_y != 0) {(
+        if (mouse.delta_x != 0 or mouse.delta_y != 0) {
             const sensitivity: f32 = 0.003;
             const delta_yaw = @as(f32, @floatFromInt(mouse.delta_x)) * sensitivity;
             const delta_pitch = -@as(f32, @floatFromInt(mouse.delta_y)) * sensitivity;
