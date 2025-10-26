af0dea27749398e7d0badd6e6337587b7cd2b326
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-25 21:10:05 -0400

    Auto-commit: Sat Oct 25 21:10:05 2025

--- src/main.zig
+++ src/main.zig
@@ -134,7 +134,14 @@ fn runSingleplayer3D(allocator: std.mem.Allocator, engine: *Engine.Engine) !void
             }
         }
 
-        const mouseInput = try Engine.readMouse(); 
+        if (try Engine.readMouseInput()) |mouseInput| {
+            if (mouseInput.?.button == 1) {
+                const sensitivity: f32 = 0.003;
+                const delta_yaw = @as(f32, @floatFromInt(mouseInput.?.delta_x)) * sensitivity;
+                const delta_pitch = -@as(f32, @floatFromInt(mouseInput.?.delta_y)) * sensitivity;
+                cam3d.rotate(delta_yaw, delta_pitch);
+            }
+        }
         if (mouseInput.?.delta_x != 0 or mouseInput.?.delta_y != 0) {
             const sensitivity: f32 = 0.003;
             const delta_yaw = @as(f32, @floatFromInt(mouseInput.?.delta_x)) * sensitivity;
