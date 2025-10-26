118428afd34582198cdb20a2e79c535fb2a63348
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-25 20:57:57 -0400

    Auto-commit: Sat Oct 25 20:57:57 2025

--- src/main.zig
+++ src/main.zig
@@ -102,7 +102,7 @@ fn runSingleplayer3D(allocator: std.mem.Allocator, engine: *Engine.Engine) !void
         }
     }
     try engine.canvas.flushToTerminal();
-    std.Thread.sleep(10 * std.time.ns_per_s);
+    std.Thread.sleep(1 * std.time.ns_per_s);
 
     const render_distance: i32 = switch (quality) {
         .Low => 20,
