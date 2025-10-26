7b75195b593f30848c21f8091de2814baa2ac9b3
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-26 12:37:11 -0400

    Auto-commit: Sun Oct 26 12:37:11 2025

--- src/main.zig
+++ src/main.zig
@@ -76,7 +76,6 @@ fn runSingleplayer3D(allocator: std.mem.Allocator, engine: *Engine.Engine) !void
         player,
     );
     
-    world.CHUNK_WIDTH = 4;
     defer world.deinit();
 
     var cam3d = Engine3D.Camera3D.init(@intCast(canvas3d.width), @intCast(canvas3d.height));
