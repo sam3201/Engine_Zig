6abe557916a5992e9a8c63044ff70dbab72f1b81
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-26 12:48:14 -0400

    Auto-commit: Sun Oct 26 12:48:14 2025

--- src/main.zig
+++ src/main.zig
@@ -11,8 +11,8 @@ const WorldManager = @import("WorldManager.zig");
 const Menu = @import("Menu.zig").Menu;
 const Thread = std.Thread;
 
-pub var WIDTH = 150;
-pub var HEIGHT = 50;
+pub const WIDTH = 150;
+pub const HEIGHT = 50;
 
 fn qualityMenu(engine: *Engine.Engine) !u8 {
     const items = [_][]const u8{
