fccbb445fbdf8a937bbfb393987d3dbc0f3963d4
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-25 20:29:33 -0400

    Auto-commit: Sat Oct 25 20:29:33 2025

--- src/Particle.zig
+++ src/Particle.zig
@@ -118,7 +118,7 @@ pub const ParticleField = struct {
     pub fn init(allocator: std.mem.Allocator, quality: ParticleQuality) !ParticleField {
         return ParticleField{
             .allocator = allocator,
-            .particles = std.ArrayList(Particle).initCapacity(allocator, 0),
+            .particles = std.ArrayList(Particle).initBuffer(allocator, 0),
             .quality = quality,
         };
     }
