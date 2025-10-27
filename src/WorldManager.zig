b1fc5bc4adc7a3d13aff303570727b055bd8a249
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-26 21:49:33 -0400

    Auto-commit: Sun Oct 26 21:49:33 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -105,7 +105,7 @@ pub const WorldManager = struct {
     pub fn updateChunks(self: *WorldManager) !void {
         const player_chunk = self.getPlayerChunkCoord();
 
-        var y: usize = player_chunk.y - self.loaded_radius;
+        var y: i32= player_chunk.y - self.loaded_radius;
         while (y <= player_chunk.y + self.loaded_radius) : (y += 1) {
             var x: usize = player_chunk.x - self.loaded_radius;
             while (x <= player_chunk.x + self.loaded_radius) : (x += 1) {
