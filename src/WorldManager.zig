240f2854d4b1f8a1638a72e9454a0857c7806157
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-27 13:58:49 -0400

    Auto-commit: Mon Oct 27 13:58:49 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -231,8 +231,8 @@ pub const WorldManager = struct {
     pub fn updateCamera(self: *WorldManager) void {
         const pos = self.player.getPosition();
 
-        self.camera_x: i32 = pos.x - @divTrunc(self.canvas_width, 2);
-        self.camera_y: i32 = pos.y - @divTrunc(self.canvas_height, 2);
+        self.camera_x = pos.x - @intCast@divTrunc(self.canvas_width, 2));
+        self.camera_y = pos.y - @divTrunc(self.canvas_height, 2);
     }
 
     fn MovePlayer(self: *WorldManager, dx: i32, dy: i32) void {
