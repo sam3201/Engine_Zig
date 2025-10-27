0ec1357553c04f08086379eeffabbb8960c8bec4
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-27 13:51:50 -0400

    Auto-commit: Mon Oct 27 13:51:50 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -351,6 +351,7 @@ pub const Chunk = struct {
             return .Stone; 
         }
 
+        const CHUNK_WIDTH_i32: i32 = @intCast(CHUNK_WIDTH);
         const idx: i32 = local_y * @intCast(CHUNK_WIDTH) + local_x;
         return self.tiles[idx];
     }
