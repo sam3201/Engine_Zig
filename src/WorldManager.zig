c085ed42001c805a97dab24c4d1f82fcd56d4dae
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-27 13:48:17 -0400

    Auto-commit: Mon Oct 27 13:48:17 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -104,12 +104,12 @@ pub const WorldManager = struct {
 
     pub fn updateChunks(self: *WorldManager) !void {
         const player_chunk = self.getPlayerChunkCoord();
-        const radius_i32: i32 = @intCast(self.loaded_radius);
+        const loaded_radius_i32: i32 = @intCast(self.loaded_radius);
 
-        var y: i32 = player_chunk.y - self.loaded_radius;
-        while (y <= player_chunk.y + self.loaded_radius) : (y += 1) {
-            var x: i32 = player_chunk.x - self.loaded_radius;
-            while (x <= player_chunk.x + self.loaded_radius) : (x += 1) {
+        var y: i32 = player_chunk.y - loaded_radius_i32;
+        while (y <= player_chunk.y + loaded_radius_i32) : (y += 1) {
+            var x: i32 = player_chunk.x - loaded_radius_i32;
+            while (x <= player_chunk.x + loaded_radius_i32) : (x += 1) {
                 const coord = Chunk.ChunkCoord{ .x = x, .y = y };
                 if (!self.chunks.contains(coord)) {
                     const player_level: u64 = @intCast(self.player.level);
