54715d39929a64577e8f5e8459c2abbe0fba44c6
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-26 21:44:28 -0400

    Auto-commit: Sun Oct 26 21:44:28 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -151,7 +151,7 @@ pub const Chunk = struct {
 
         try self.items.append(allocator, WorldItem{
             .item = Inventory.Item.initConsumable(.Potion, 1, "Potion", allocator),
-            .x = self.coord.x * CHUNK_WIDTH + 2,
+            .x = self.coord.x * @as(i32,CHUNK_WIDTH) + 2,
             .y = self.coord.y * CHUNK_HEIGHT + 2,
             .ch = 'P',
             .color = Engine.Color{ .r = 200, .g = 0, .b = 200 },
