3d345acf1cf6ee96c0e292c11be4f4235dd25c53
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-26 21:28:23 -0400

    Auto-commit: Sun Oct 26 21:28:23 2025

--- src/Chunk.zig
+++ src/Chunk.zig
@@ -97,7 +97,7 @@ pub const ChunkCoord = struct {
 
     pub fn hash(self: ChunkCoord) usize {
         const x_hash: usize = @bitCast(self.x);
-        const y_hash: u64 = @bitCast(@as(i64, self.y));
+        const y_hash: usize = @bitCast(self.y); 
         return x_hash ^ (y_hash << 1);
     }
 
