413d358d60a70dd7927165cf6677acb451f299da
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-26 21:33:12 -0400

    Auto-commit: Sun Oct 26 21:33:12 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -18,7 +18,7 @@ pub const WorldManager = struct {
     player: Player.Player,
     Players: std.ArrayList(Player.Player), 
     chunks: std.HashMap(Chunk.ChunkCoord, Chunk.Chunk, ChunkContext, std.hash_map.default_max_load_percentage),
-    loaded_radius: i32 = 2,
+    loaded_radius: usize = 2,
     canvas_width: i32,
     canvas_height: i32,
     camera_x: i32 = 0,
