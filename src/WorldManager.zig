b0449bab97c5869f8fa1c3b6e33c86ac9cf09bfa
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-10-27 13:55:23 -0400

    Auto-commit: Mon Oct 27 13:55:23 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -219,8 +219,8 @@ pub const WorldManager = struct {
         }
 
         const new_pos = self.player.getPosition();
-        if (@divFloor(old_pos.x, @as(i32, CHUNK_WIDTH)) != @divFloor(new_pos.x, @as(i32, CHUNK_WIDTH)) or
-            @divFloor(old_pos.y, @as(i32, CHUNK_HEIGHT)!= @divFloor(new_pos.y, @as(i32, CHUNK_HEIGHT))
+        if (@divFloor(old_pos.x, @as(i32, CHUNK_WIDTH)) != @divFloor(new_pos.x, CHUNK_WIDTH) or
+            @divFloor(old_pos.y, CHUNK_HEIGHT) != @divFloor(new_pos.y, CHUNK_HEIGHT))
          {
              try self.updateChunks();
          }
