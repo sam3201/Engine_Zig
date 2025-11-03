bf716890cfdc1d67cfe1fb6fa15c054d7f35da46
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-11-03 13:51:40 -0500

    Auto-commit: Mon Nov  3 13:51:40 2025

--- src/WorldManager.zig
+++ src/WorldManager.zig
@@ -686,9 +686,12 @@ pub fn renderParticleField3D(
                 const b: u8 = @intFromFloat(@as(f32, @floatFromInt(base_color.b)) * fade);
                 
                 const ch: u8 = if (is_ground) {
-                    if (distance < 5.0) '█' else if (distance < 10.0) '▓' else if (distance < 15.0) '▒' else '░';
+                    if (distance < 5.0) '█' {
+                        ' ';
+                        
+                    } else if (distance < 10.0) '▓' else if (distance < 15.0) '▒' else '░' 
                 } else {
-                    if (distance < 5.0) '#' else if (distance < 10.0) '@' else if (distance < 20.0) '+' else '.';
+                    if (distance < 5.0) '#' else if (distance < 10.0) '@' else if (distance < 20.0) '+' else '.'
                 };
                 
                 canvas3D.put(screen_x, screen_y, ch);
