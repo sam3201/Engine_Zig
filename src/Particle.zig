e55eacb1afb7b1fbab81fa0ecdc95923b93f9022
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-11-03 13:46:00 -0500

    Auto-commit: Mon Nov  3 13:46:00 2025

--- src/Particle.zig
+++ src/Particle.zig
@@ -24,25 +24,50 @@ pub const Particle = struct {
     }
     
     fn getColorForTile(tile_type: Chunk.TileType, height: f32) Engine3D.Color3D {
-        return switch (tile_type) {
-            .Empty => Engine3D.Color3D.init(64, 64, 64),
-            .Grass => if (height > 0.5) 
-                Engine3D.Color3D.init(50, 200, 50)  // Bright grass on top
-            else 
-                Engine3D.Color3D.init(101, 67, 33),  // Dirt below
-            .Stone => Engine3D.Color3D.init(128, 128, 128),
-            .Water => Engine3D.Color3D.init(30, 100, 200),
-            .Tree => if (height > 3.0)
-                Engine3D.Color3D.init(0, 150, 0)     // Leaves
-            else
-                Engine3D.Color3D.init(101, 67, 33),  // Trunk
-            .Mountain => Engine3D.Color3D.init(100, 100, 100),
-            .Desert => Engine3D.Color3D.init(220, 180, 100),
-            .Snow => Engine3D.Color3D.init(255, 255, 255),
-            .Lava => Engine3D.Color3D.init(255, 100, 0),
-            .Wall => Engine3D.Color3D.init(139, 90, 43),
-        };
-    }
+    return switch (tile_type) {
+        .Empty => if (height < 0.1)
+            Engine3D.Color3D.init(40, 40, 40)  // Dark ground
+        else
+            Engine3D.Color3D.init(64, 64, 64),
+        .Grass => if (height < 0.1) 
+            Engine3D.Color3D.init(34, 139, 34)  // Forest green ground
+        else if (height > 0.5) 
+            Engine3D.Color3D.init(50, 200, 50)  // Bright grass on top
+        else 
+            Engine3D.Color3D.init(101, 67, 33),  // Dirt in middle
+        .Stone => if (height < 0.1)
+            Engine3D.Color3D.init(80, 80, 80)  // Dark stone ground
+        else
+            Engine3D.Color3D.init(128, 128, 128),
+        .Water => Engine3D.Color3D.init(30, 100, 200),
+        .Tree => if (height > 3.0)
+            Engine3D.Color3D.init(0, 150, 0)     // Leaves
+        else if (height < 0.5)
+            Engine3D.Color3D.init(139, 90, 43)   // Trunk base
+        else
+            Engine3D.Color3D.init(101, 67, 33),  // Trunk middle
+        .Mountain => if (height < 0.1)
+            Engine3D.Color3D.init(70, 70, 70)   // Mountain base
+        else
+            Engine3D.Color3D.init(100, 100, 100),
+        .Desert => if (height < 0.1)
+            Engine3D.Color3D.init(194, 178, 128)  // Sand ground
+        else
+            Engine3D.Color3D.init(220, 180, 100),
+        .Snow => if (height < 0.1)
+            Engine3D.Color3D.init(240, 248, 255)  // Snow ground
+        else
+            Engine3D.Color3D.init(255, 255, 255),
+        .Lava => if (height < 0.1)
+            Engine3D.Color3D.init(139, 0, 0)      // Dark lava ground
+        else
+            Engine3D.Color3D.init(255, 100, 0),
+        .Wall => if (height < 0.1)
+            Engine3D.Color3D.init(101, 67, 33)    // Brown ground
+        else
+            Engine3D.Color3D.init(139, 90, 43),
+    };
+}
 };
 
 pub const Vec3 = struct {
